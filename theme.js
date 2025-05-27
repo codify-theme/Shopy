996be72fc6379c9e2520dad99d2f2d786d5e1a84
taslima97 <antora.taslima97@gmail.com>
2025-05-28 01:44:51 +0700

    intro1

---
+++ theme.js
@@ -0,0 +1,15 @@
+
+// Intro1  Slider Style 
+var oneGridBannerSlider = new Swiper('.one-grid-pagination-slider', {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    loop: true,
+    autoplay: true,
+    centeredSlides: true,
+    speed: 800,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
+
